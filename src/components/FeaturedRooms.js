d0a01645841238f4362cbe466aa73f063b1d0c42
Stephen Axtell <axtell.stephen@gmail.com>
2019-12-27 11:38:35 +0200

    adding loading spinner plus logic as well as new title component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 import { RoomContext } from '../context';
+import Loading from './Loading';
+import Room from './Room';
+import Title from './Title';
 
 export class FeaturedRooms extends Component {
 
@@ -8,10 +11,20 @@ export class FeaturedRooms extends Component {
 	static contextType = RoomContext;
 
 	render() {
-		const { featuredRooms: rooms} = this.context;
-		console.log(rooms);
-		
-		return <div>hello from featured rooms</div>;
+		// featuredRooms: rooms means alias of featuredRooms is rooms
+		let {loading, featuredRooms: rooms} = this.context;
+		rooms = rooms.map(room => {
+			return <Room key={room.id} room={room} />
+		});
+				
+		return (
+			<section className="featured-rooms">
+				<Title title="featured rooms" />
+				<div className="featured-rooms-center">
+					{loading ? <Loading /> : rooms}
+				</div>				
+			</section>
+		);
 	}
 }
 
