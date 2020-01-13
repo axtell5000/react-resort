5d00a74691503c6eb9ec35a0e9679ae3c8fbf693
Stephen Axtell <axtell.stephen@gmail.com>
2020-01-13 09:31:56 +0200

    setting up COntext for SingleRoom

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import defaultBack from '../images/room-3.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {RoomContext} from '../context';
 
 export class SingleRoom extends Component {
+	constructor(props) {
+		super(props);
+		// console.log(this.props);
+		this.state = {
+			slug: this.props.match.params.slug,
+			defaultBack
+		};
+		
+	}
+
+	static contextType = RoomContext;
+
+	// componentDidMount() {}
+
 	render() {
+		const {getRoom} = this.context;
+		const room = getRoom(this.state.slug);
+		if (!room) {
+			return <div className="error">
+				<h3>no such room can be found...</h3>
+				<Link to="/rooms" className="btn-primary">back to rooms</Link>
+			</div>
+		}
+		
 		return (
 			<div>
-				Hello from single room
+				Hello from single room {room.name}
 			</div>
 		);
 	}
