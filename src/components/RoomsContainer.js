fae11e5680742a0b0afd8b93d55ff3c75e2768a6
Stephen Axtell <axtell.stephen@gmail.com>
2020-01-13 14:55:52 +0200

    setting up context for the Rooms Container

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
+import { RoomConsumer } from '../context';
+import Loading from './Loading';
 import RoomsFilter from './RoomsFilter';
 import RoomsList from './RoomsList';
 
+// below is to show how to work with context in a functional component, one can also use a higher order component
 function RoomsContainer() {
 	return (
-		<div>
-			Hello from RoomsContainer
-			<RoomsFilter />
-			<RoomsList />
-		</div>
+		<RoomConsumer>
+			{value => {
+				const{loading, sortedRooms, rooms} = value;
+				
+				if (loading) {
+					return <Loading />
+				}
+
+				return (
+					<>						
+						<RoomsFilter rooms={rooms} />
+						<RoomsList rooms={sortedRooms} />
+					</>
+				);
+			}}
+		</RoomConsumer>
 	);
 }
 
